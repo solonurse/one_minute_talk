d72a1b97a6f7d78a35e7f5d195a219abd50e4df2
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2023-12-23 23:48:35 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/top'
+  get 'static_pages/terms'
+  get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
   
   # Defines the root path route ("/")
-  root "home#top"
-
-  get 'home/top'
+  root "static_pages#top"
 end
