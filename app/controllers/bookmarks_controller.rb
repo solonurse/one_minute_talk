a192e8a5067ede9255607c199476c9382e398877
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-01-18 21:54:59 +0900

    メモの削除機能の追加、ブックマーク機能の修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,15 @@
 class BookmarksController < ApplicationController
   def create
-    @memo = Memo.find(params[:memo_id])
+    @memo = current_user.memos.find(params[:memo_id])
     current_user.bookmark(@memo)
+    @bookmark_memos = current_user.bookmark_memos.all.order(created_at: :desc)
 
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.replace("bookmark-button-for-memo-#{@memo.id}", partial: "memos/unbookmark", locals: {memo: @memo}),
-          turbo_stream.replace("slidebar-bookmark-button-for-memo-#{@memo.id}", partial: "memos/slidebar/slidebar_unbookmark", locals: {memo: @memo})
+          turbo_stream.replace("slidebar-bookmark-button-for-memo-#{@memo.id}", partial: "memos/slidebar/slidebar_unbookmark", locals: {memo: @memo}),
+          turbo_stream.replace("bookmarked-memo", partial: "memos/bookmarked_memo", locals: {bookmark_memos: @bookmark_memos}),
         ]
       end
     end
@@ -16,24 +18,16 @@ class BookmarksController < ApplicationController
   def destroy
     @memo = current_user.bookmarks.find(params[:id]).memo
     current_user.unbookmark(@memo)
+    @bookmark_memos = current_user.bookmark_memos.all.order(created_at: :desc)
 
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.replace("unbookmark-button-for-memo-#{@memo.id}", partial: "memos/bookmark", locals: {memo: @memo}),
-          turbo_stream.replace("slidebar-unbookmark-button-for-memo-#{@memo.id}", partial: "memos/slidebar/slidebar_bookmark", locals: {memo: @memo})
+          turbo_stream.replace("slidebar-unbookmark-button-for-memo-#{@memo.id}", partial: "memos/slidebar/slidebar_bookmark", locals: {memo: @memo}),
+          turbo_stream.replace("bookmarked-memo", partial: "memos/bookmarked_memo", locals: {bookmark_memos: @bookmark_memos}),
         ]
       end
     end
   end
-
-  # def create_slidebar
-  #   @memo = Memo.find(params[:memo_id])
-  #   current_user.bookmark(@memo)
-  # end
-
-  # def destroy_slidebar
-  #   @memo = current_user.bookmarks.find(params[:id]).memo
-  #   current_user.unbookmark(@memo)
-  # end
 end
