17c20937e76cffe4148ee6ff1cf2a59b4666a136
中野駿介 <nakanoshunsuke@nakanoshunsukenoMacBook-Air.local>
2024-03-02 23:41:27 +0900

    リマインダーのコントローラ作成

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,16 +2,36 @@ class RemindersController < ApplicationController
   before_action :reminder_params
 
   def create
-    
+    if @schedule.present?
+      reminder = Reminder.new(user_id: current_user.id, memo_id: session[:memo_id], start_at: @schedule, reminder: true)
+      if reminder.save
+        flash[:success] = 'リマインダーを登録しました'
+      else
+        flash[:danger] = 'リマインダーを登録できませんでした'
+      end
+      redirect_to memo_path(session[:memo_id])
+    else
+      redirect_to memo_path(session[:memo_id]), danger: '予定日を設定してください'
+    end
   end
 
   def update
+    if @schedule.present?
+      reminder = Reminder.find_by(id: params[:id])
+      if reminder.update(start_at: @schedule)
+        flash[:success] = 'リマインダーを更新しました'
+      else
+        flash[:danger] = 'リマインダーを更新できませんでした'
+      end
+      redirect_to memo_path(session[:memo_id])
+    else
+      redirect_to memo_path(session[:memo_id]), danger: '予定日を設定してください'
+    end
   end
 
   private
 
   def reminder_params
-    @time = params.require(:reminder).permit(:started_at)
-    @memo = Reminder.find(params[:id]).memo
+    @schedule = params[:reminder][:started_at]
   end
 end
